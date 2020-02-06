9d9f62ccd9250f17fc2e039babae25af19841cfd
jaber babaki <jaber.babaki94@gmail.com>
2020-02-06 20:21:29 +0330

    finish euler plot and exact

--- advection-1Deuler.py
+++ advection-1Deuler.py
@@ -2,33 +2,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-i=np.arange(-10,10,0.01)
-j=np.arange(-10,10,0.01)
+x=np.arange(-5,5,0.1)
+t=np.arange(0,7,0.1)
+u_1=np.zeros([len(t),len(x)])
 
-u_1=np.zeros([len(j),len(i)])
-
-
-def exact_solution(j,x):
-    v=0
-    t=0
-    u_1[0,j]=np.exp(-((x+0.3)**2-v*t))
-
-
-def euler_scheme1(n,l,j,i):
-    v=0.5
-    delta_t=0.05
-    delta_x=0.05
-    a=(v*delta_t)/(2*delta_x)
-    u_1[n,l]=u_1[j+1,i]-a*(u_1[j+2,i]-u_1[j,i])
-    #print(u_1[j,i+1])
-
-
-t=0
-for z in np.arange(-10,10,0.01):
-    exact_solution(t,z)
+v=0.5
+t1=0;
+j2=0
+for j in np.arange(0,7,0.1):
+    x2=0
+    for x1 in np.arange(-5,5,0.1):
+        u_1[j2,x2]=np.exp(-((x1+0.3-v*t1)**2))
+        x2=x2+1
     t=t+1
+    j2=j2+1
+#print(u_1)
+#plt.plot(x,u_1[0,:])
+#plt.show()
 
-plt.plot(j,u_1[0,:])
-plt.show()
+
+
+x=np.arange(-5,5,0.1)
+t=np.arange(0,7,0.1)
+u_2=np.zeros([len(t),len(x)])
+t1=0;
+v=0.5
+x2=0
+for x1 in np.arange(-5,5,0.1):
+    u_2[0,x2]=np.exp(-((x1+0.3-v*t1)**2))
+    x2=x2+1
+#print(u_2)
+#plt.plot(x,u_2[0,:])
+#plt.show()
+
+
+
+v=0.5
+delta_t=0.05
+delta_x=0.05
+a=(v*delta_t)/(2*delta_x)
+l=1
+for g in np.arange(len(t)-1):
+    n=1
+    for s in np.arange(len(x)-2):
+        u_2[l,n]=u_2[g,s+1]-a*(u_2[g,s+2]-u_2[g,s])
+        #u_2[l,n]=u_2[g,s+1]+u_2[g,s+2]+u_2[g,s]
+        #print(n,l,s,0)
+        n=n+1
+    u_2[l,0]=u_2[l,len(x)-2]   #i
+    u_2[l,len(x)-1]=u_2[l,1]   #i
+    l=l+1
 print(u_1)
+print("******")
+print(u_2)
 
+plt.plot(x,u_1[5,:])
+plt.plot(x,u_2[5,:])
+plt.show()
+
+print(1.84519524e-01-a*(5.04176026e-03-9.13931185e-01))
